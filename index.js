3aef0d0c804bbf9ee0e939c0724d1090dd245654
System <none>
2017-09-25 19:06:39 +0000

    Checkpoint

---
+++ index.js
@@ -0,0 +1,64 @@
+// Imports dependencies
+const 
+  express = require('express'),
+  bodyParser = require('body-parser');
+
+// Creates an express HTTP server
+let app = express();
+app.use(bodyParser.urlencoded({"extended": false}));
+app.use(bodyParser.json());
+
+// Sets server port and logs message on success
+app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
+
+// Creates the endpoint for our webhook 
+app.post('/webhook', (req, res) => {  
+ 
+  let body = req.body;
+
+  // Checks this is a page subscription
+  if (body.object === 'page') {
+
+    // Iterates over each entry - there may be multiple if batched
+    body.entry.forEach(function(entry) {
+
+      // Gets the message. entry.messaging is an array, but 
+      // will only ever contain one message, so we get index 0
+      let message = JSON.stringify(entry.messaging[0].message);
+      console.log(message);
+    });
+  }
+
+  // Returns a '200 OK' response to all requests
+  res.send('EVENT_RECEIVED').status(200);
+
+});
+
+// Adds support for GET requests to our webhook
+app.get('/webhook', (req, res) => {
+
+  // Your verify token. Should be a random string.
+  let VERIFY_TOKEN = "<YOUR_VERIFY_TOKEN>"
+    
+  // Parse the query params
+  let mode = req.query['hub.mode'];
+  let token = req.query['hub.verify_token'];
+  let challenge = req.query['hub.challenge'];
+    
+  // Checks if a token and mode is in the query string of the request
+  if (mode && token) {
+  
+    // Checks the mode and token sent is correct
+    if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+      
+      // Responds with the challenge token from the request
+      console.log('WEBHOOK_VERIFIED');
+      res.send(challenge);
+    
+    } else {
+
+      // Responds with an error if the tokens do not match
+      res.send('Error, wrong validation token');    
+    }
+  }
+});
