c01c977ea5e42eb3989ca5607405ccc5c3891232
Glitch (messenger-platform-quick-start) <none>
2021-10-29 19:51:50 +0000

    🎑✊ Checkpoint ./app.js:38133676/1319 ./README.md:38133676/1055

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 /*
- * Starter Project for Messenger Platform Quick Start Tutorial
+ * Starter Project for WhatsApp Webhook Tutorial
  *
- * Remix this as the starting point for following the Messenger Platform
- * quick start tutorial.
+ * Remix this as the starting point for following the WhatsApp Getting Started tutorial 
  *
- * https://developers.facebook.com/docs/messenger-platform/getting-started/quick-start/
+ * https://developers.facebook.com/docs/whatsapp/getting-started/signing-up/
  *
  */
 
@@ -25,23 +24,13 @@ app.post('/webhook', (req, res) => {
 
   // Parse the request body from the POST
   let body = req.body;
-
-  // Check the webhook event is from a Page subscription
-  if (body.object === 'page') {
-
-    // Iterate over each entry - there may be multiple if batched
-    body.entry.forEach(function(entry) {
-
-      // Get the webhook event. entry.messaging is an array, but 
-      // will only ever contain one event, so we get index 0
-      let webhook_event = entry.messaging[0];
-      console.log(webhook_event);
-      
-    });
-
-    // Return a '200 OK' response to all events
-    res.status(200).send('EVENT_RECEIVED');
-
+  
+  // Check the Incoming webhook message
+  console.log("Incoming webhook: " + JSON.stringify(request.body));
+  
+  // Validate the webhook
+  if(request.body.statuses){
+    res.sendStatus(200);
   } else {
     // Return a '404 Not Found' if event is not from a page subscription
     res.sendStatus(404);
