11cd47a72f0a46b16a2c7461e61a73a6a3b748c3
Glitch (messenger-platform-quick-start) <none>
2022-05-03 02:11:38 +0000

    🎮🐃 Checkpoint ./app.js:38126137/9381

--- app.js
+++ app.js
@@ -7,13 +7,16 @@
 
 "use strict";
 
+// access token for your app (copy from DevX getting started page)
+const token =
+  "EAAAO6uZBxesMBAOZBZBlDNPZA0rPXDdrRumSBHyn95uoJDA1akwDZAKggZAZCgv3PWHLmwMQk21MyeZCgxlSiSvXaeXf8PV9frjmh8dsr85PhSs9faOKZBT78ZARQAAc8jZBbCZAXYZALjj0Pw8NQ516mZAtRH9wFToy1iFWUZB2muw5MX3NgE729j43usIxgzBdl9G70eNdyC5nje188ysqEDycla8xHVwoWlUGVxhLSpGeWZBvRIZAGjkez3pmG0Uns7ehq3ayaLktevi9TQHiPdMBpHFamOO3mEfHqjusZD";
+
 // Imports dependencies and set up http server
 const request = require("request"),
   express = require("express"),
   body_parser = require("body-parser"),
-  axios = require('axios').default,
+  axios = require("axios").default,
   app = express().use(body_parser.json()); // creates express http server
-
 
 // Sets server port and logs message on success
 app.listen(process.env.PORT || 1337, () => console.log("webhook is listening"));
@@ -25,38 +28,26 @@ app.post("/webhook", (req, res) => {
 
   // Check the Incoming webhook message
   console.log(JSON.stringify(req.body, null, 2));
-  
-  
-  
-  
-    // access token for your app (copy from DevX getting started page)
-  const token = 'EAAAO6uZBxesMBAOZBZBlDNPZA0rPXDdrRumSBHyn95uoJDA1akwDZAKggZAZCgv3PWHLmwMQk21MyeZCgxlSiSvXaeXf8PV9frjmh8dsr85PhSs9faOKZBT78ZARQAAc8jZBbCZAXYZALjj0Pw8NQ516mZAtRH9wFToy1iFWUZB2muw5MX3NgE729j43usIxgzBdl9G70eNdyC5nje188ysqEDycla8xHVwoWlUGVxhLSpGeWZBvRIZAGjkez3pmG0Uns7ehq3ayaLktevi9TQHiPdMBpHFamOO3mEfHqjusZD';
-  // phone number id of your phone number you are sending from (copy from DevX getting started page)
-  const num_id = '26902205832071';
-    
-  if (req.body.entry && req.body.entry[0].changes && req.body.entry[0].changes[0] && req.body.entry[0].changes[0].value.messages && req.body.entry[0].changes[0].value.messages[0] ) {
-    let from = req.body.entry[0].changes[0].value.messages[0].from; // extract the phone number from the webhook payload
-    let msg_body = req.body.entry[0].changes[0].value.messages[0].text.body; // extract the message text from the webhook payload
-    axios({
-      method: 'POST', // Required, HTTP method, a string, e.g. POST, GET
-      url: "https://graph.facebook.com/v12.0/"+num_id+"/messages\?access_token=" + token,
-      data: { "messaging_product": "whatsapp", "to": from, "text": {"body" : "ack: " + msg_body} },
-      headers: { "Content-Type": "application/json" }
-    });
-  }
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
 
   // Validate the webhook
   if (req.body.object) {
+    if (
+      req.body.entry && req.body.entry[0].changes && req.body.entry[0].changes[0] && req.body.entry[0].changes[0].value.messages && req.body.entry[0].changes[0].value.messages[0]
+    ) {
+      let phone_number_id = req.body.entry[0].changes[0].value.metadata.phone_number_id;
+      let from = req.body.entry[0].changes[0].value.messages[0].from; // extract the phone number from the webhook payload
+      let msg_body = req.body.entry[0].changes[0].value.messages[0].text.body; // extract the message text from the webhook payload
+      axios({
+        method: "POST", // Required, HTTP method, a string, e.g. POST, GET
+        url: "https://graph.facebook.com/v12.0/" + phone_number_id + "/messages?access_token=" + token,
+        data: {
+          messaging_product: "whatsapp",
+          to: from,
+          text: { body: "ack: " + msg_body },
+        },
+        headers: { "Content-Type": "application/json" },
+      });
+    }
     res.sendStatus(200);
   } else {
     // Return a '404 Not Found' if event is not from a whatsApp API
@@ -87,5 +78,4 @@ app.get("/webhook", (req, res) => {
       res.sendStatus(403);
     }
   }
-  
 });
