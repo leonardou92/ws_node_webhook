93d401e7a218489f0cf9073c508158b7d93c3e07
Glitch (iridescent-enthusiastic-word) <none>
2022-06-18 19:45:57 +0000

    🏂👕 Checkpoint ./app.js:45974667/142

--- app.js
+++ app.js
@@ -86,7 +86,6 @@ app.get("/webhook", (req, res) => {
       // Respond with 200 OK and challenge token from the request
       console.log("WEBHOOK_VERIFIED");
       res.status(200).send(challenge);
-      res.send("hola");
     } else {
       // Responds with '403 Forbidden' if verify tokens do not match
       res.sendStatus(403);
