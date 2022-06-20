1133f2975409039ac83497786aaaa03613f37f1f
Glitch (iridescent-enthusiastic-word) <none>
2022-06-20 20:43:34 +0000

    🎋🍶 Checkpoint ./app.js:45974667/889

--- app.js
+++ app.js
@@ -30,13 +30,16 @@ app.post("/webhook", (req, res) => {
   // Check the Incoming webhook message
   console.log(JSON.stringify(req.body, null, 2));
   const my_json = JSON.stringify(req.body, null, 2);
-  
-  async function doPostRequest(my_json){
-    let resp = await axios.post('http://scryptcase.tecnovenca.net:8091/scriptcase/app/webservice/ws_web/', my_json);
-    let data = resp.data;
-    console.log(data);
+  const doPostRequest = async () => {
+    try {
+        const resp = await axios.post('http://scryptcase.tecnovenca.net:8091/scriptcase/app/webservice/ws_web/', my_json);
+        console.log(resp.data);
+    }     
+    catch(error) {
+      res.status(error.response.status)
+      return res.send(error.message);
+    })
   }
-
   doPostRequest();
   
   // info on WhatsApp text message payload: https://developers.facebook.com/docs/whatsapp/cloud-api/webhooks/payload-examples#text-messages
