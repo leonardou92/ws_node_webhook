868acbe422983ef19c71dbb20683b529fffae5b5
Glitch (iridescent-enthusiastic-word) <none>
2022-06-23 14:36:23 +0000

    👔🌲 Checkpoint ./app.js:45974667/909

--- app.js
+++ app.js
@@ -10,7 +10,7 @@
 // Access token for your app
 // (copy token from DevX getting started page
 // and save it as environment variable into the .env file)
-const token = process.env.WHATSAPP_TOKEN;
+//const token = process.env.WHATSAPP_TOKEN;
 
 // Imports dependencies and set up http server
 const request = require("request"),
@@ -24,6 +24,7 @@ app.listen(process.env.PORT || 1337, () => console.log("webhook is listening"));
 
 // Accepts POST requests at /webhook endpoint
 app.post("/webhook", (req, res) => {
+  
   // Parse the request body from the POST
   let body = req.body;
 
@@ -187,7 +188,7 @@ app.get("/webhook", (req, res) => {
 
   // Parse params from the webhook verification request
   let mode = req.query["hub.mode"];
-  let token = req.query["hub.verify_token"];
+  const token = req.query["hub.verify_token"];
   let challenge = req.query["hub.challenge"];
   
   // Check if a token and mode were sent
